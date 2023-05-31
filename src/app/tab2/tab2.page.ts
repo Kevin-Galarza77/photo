fd5b1226a5b96af56dd5ed1d63c56053748110dd
Kevin-Galarza77 <jimenezkev1040@gmail.com>
2023-05-31 11:03:42 -0500

    Crear imagen, almacenarla y tambien eliminarla

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
+import { Preferences } from '@capacitor/preferences';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   constructor(public photoService: PhotoService) { }
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
+
+  deletePhoto(position: number) {
+    // Eliminar la imagen del arreglo de fotos
+    this.photoService.photos.splice(position, 1);
+
+    // Guardar el nuevo arreglo de fotos en las preferencias
+    Preferences.set({
+      key: this.photoService.PHOTO_STORAGE,
+      value: JSON.stringify(this.photoService.photos),
+    });
+  }
+
 
 }
